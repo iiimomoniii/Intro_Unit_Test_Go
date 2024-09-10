a990f5c5998f66fafd3ad7d050ae8de1048d190a
natdanai.tha <natdanai.tha@cjexpress.co.th>
2024-09-10 19:01:39 +0700

    Update
    test by mockup method

--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"gotest/utils"
+
+	"github.com/stretchr/testify/mock"
+)
 
 func main() {
-	fmt.Println("Hello World")
+
+	c := CustomerRepositoryMock{}
+	//"Bond", 18, nil => index of Bond = 0 , index of 18 = 1 , index of nil = 2
+	c.On("GetCustomer", 1).Return("Bond", 18, nil)
+	c.On("GetCustomer", 2).Return("", 0, utils.ErrNotFound)
+
+	//case GetCustomer = 1
+	//run
+	//go run .
+	// name, age, err := c.GetCustomer(1)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// fmt.Println(name, age)
+
+	//case GetCustomer = 2
+	//run
+	//go run .
+	name, age, err := c.GetCustomer(2)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(name, age)
+
+}
+
+type CustomerRepository interface {
+	GetCustomer(id int) (name string, age int, err error)
+}
+
+type CustomerRepositoryMock struct {
+	mock.Mock
+}
+
+func (m *CustomerRepositoryMock) GetCustomer(id int) (name string, age int, err error) {
+	args := m.Called(id)
+	return args.String(0), args.Int(1), args.Error(2)
 }
--- utils/error.go
+++ utils/error.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrZeroAmount = errors.New("purchase amount could not be zero")
 	ErrRepository = errors.New("repository error")
+	ErrNotFound = errors.New("not found")
 )
